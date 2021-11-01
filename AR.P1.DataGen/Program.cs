f87e740fbee352ae0e54dea744d94ec33a031977
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-11-01 21:05:49 +0100

    Add project files.

---
+++ AR.P1.DataGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AR.P1.DataGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
