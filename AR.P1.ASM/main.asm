54560f5a24ba6c52341fce9fec656c8dde7e30d2
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-11-06 03:29:24 -0400

    Temp

--- AR.P1.ASM/main.asm
+++ AR.P1.ASM/main.asm
@@ -28,8 +28,11 @@ std_out: equ 1
 section .data
     filename: db "/home/ubuntu/Documents/AR.P1-master/AR.P1.ASM/output.wav", 0
     filename_len: equ $-filename
+
     welcome: db "FFT ASM", 0
     welcome_len: equ $-welcome
+    goodbye_str: db "Finished.", 0
+    goodbye_str_len: equ $-goodbye_str
     
     format_str: db "%s", 0
     
@@ -37,39 +40,66 @@ section .data
     
     missing_args_str: db "No file name provided.", 0
     missing_args_str_len: equ $-missing_args_str
+    failed_file_open_str: db "Failed to open file.", 0
 
+    too_many_args_str: db "Too many arguments provided.", 0
     invalid_sampling_rate_str: db "Invalid sampling rate provided.", 0
     invalid_bit_depth_str: db "Invalid bit depth provided.", 0
 
 section .bss
     argc: resq 1
     argv: resq 1
+    filename_arg: resq 1
 
 section .text
 global CMAIN ;CMAIN/_start
 CMAIN:
     mov rbp, rsp; for correct debugging
 
+    mov [argv], rsi
     mov [argc], rdi
-    cmp byte [argc], 1
+    
+;    print welcome,welcome_len
+     cmp byte [argc], 1
     ;JZ missing_args
 
-   ; save argc and argv
+    mov rax, 2
+    sub rax, [argc]
+    js too_many_args
+
     mov rcx, [argc]
-    mov [argv], rsi
     mov rbx, rsi
 
-    printl welcome,welcome_len
-    printl filename, filename_len ; TODO use cli arg
+;    printl welcome, welcome_len
+
+    call get_filename
+    mov [filename_arg], rsi
+    mov rdi, [filename_arg]
+    call cout_str
+    
+ ;   printl filename, filename_len ; TODO use cli arg
 
     mov rdi, filename ; TODO use cli arg
     mov rax, sys_open
     mov rsi, O_RDONLY    
     syscall
     
+    cmp rax, 0
+    js failed_file_open
+    
+    ; TODO read file
+    
+;    printl goodbye_str, goodbye_str_len
+                            
     xor rax, rax
     ret
 
+cout_str:
+    ;str in rdi
+    call strlen_o
+    mov rdx, rax
+    call cout_str_w_len
+    ret
 cout_str_w_len:
     ;rsi -str ptr
     ;rdx - num of bytes to print
@@ -77,6 +107,10 @@ cout_str_w_len:
     mov rdi, std_out
     syscall
     ret
+failed_file_open:
+    PRINT_STRING failed_file_open_str
+    NEWLINE
+    call exit
 invalid_bit_depth:
     PRINT_STRING invalid_bit_depth_str
     NEWLINE
@@ -89,21 +123,27 @@ exit:
     mov rax, sys_exit
     xor rdi, rdi
     syscall
-print_argv:
+get_filename:    
     ;argv in rbx
+    ;argc in rcx
+    ;rsi will contain argv[0]
+    cmp rcx,0
+    
+    push rcx
     mov rdi, [rbx]
     call strlen_o
     
     mov rdx, rax
-    mov rax, sys_write
     mov rsi, rdi
-    mov rdi, std_out
-    syscall
+    call cout_str_w_len
     NEWLINE
     
-    ;TODO loop
-   
+    pop rcx
+    dec rcx
+    cmp rcx, 0
+    jnz too_many_args ; TODO set rsi to be argv[argc-1]
     ret
+get_filename_1
 strlen_o:
     ;string ptr in rdi
     push rbx
@@ -124,5 +164,9 @@ strlen_o:
     ret
 missing_args:
     PRINT_STRING missing_args_str
+    NEWLINE
+    call exit
+too_many_args:
+    PRINT_STRING too_many_args_str
     NEWLINE
     call exit
