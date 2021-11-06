b12550bbe149af00d49f4c0dbf61a295dc067bff
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-11-06 06:29:28 -0400

    Tmp

--- AR.P1.ASM/main.asm
+++ AR.P1.ASM/main.asm
@@ -9,12 +9,18 @@ extern strlen
 sys_read: equ 0
 sys_write: equ 1
 sys_open: equ 2
+sys_close: equ 3
 sys_exit: equ 60
 ; flags
 O_RDONLY: equ 0
+O_WRONLY: equ 1
 O_RDWR: equ 2
+O_CREAT: equ 100
+O_TRUNC: equ 1000
 ; i/o
+std_in: equ 0
 std_out: equ 1
+std_err: equ 2
 
 %macro print 2
     mov rsi, %1
@@ -28,8 +34,12 @@ std_out: equ 1
 %endmacro
 
 section .data
+    ;TODO move strings to section .rodata
     filename: db "/home/ubuntu/Documents/AR.P1-master/AR.P1.ASM/output.wav", 0
     filename_len: equ $-filename
+
+    out_filename: db "output.bin", 0
+    out_filename_len: equ $-out_filename
 
     welcome: db "FFT ASM", 0
     welcome_len: equ $-welcome
@@ -42,17 +52,19 @@ section .data
     
     missing_args_str: db "No file name provided.", 0
     missing_args_str_len: equ $-missing_args_str
-    failed_file_open_str: db "Failed to open file.", 0
+    failed_file_open_str: db "Failed to open file ", 0
 
     too_many_args_str: db "Too many arguments provided.", 0
     invalid_sampling_rate_str: db "Invalid sampling rate provided.", 0
     invalid_bit_depth_str: db "Invalid bit depth provided.", 0
 
+    buffer: 
 section .bss
     argc: resq 1
     argv: resq 1
     filename_arg: resq 1
-
+    fd_in: resq 1
+    fd_out: resq 1
 section .text
 global CMAIN ;CMAIN/_start
 CMAIN:
@@ -67,7 +79,7 @@ CMAIN:
 
     mov rax, 2
     sub rax, [argc]
-    js too_many_args
+    ;js too_many_args
 
     mov rcx, [argc]
     mov rbx, rsi
@@ -76,7 +88,7 @@ CMAIN:
 
     call get_filename
     mov [filename_arg], rsi
-    mov rdi, [filename_arg]
+    mov rdi, rsi
     call cout_str
     
  ;   printl filename, filename_len ; TODO use cli arg
@@ -85,17 +97,37 @@ CMAIN:
     mov rax, sys_open
     mov rsi, O_RDONLY    
     syscall
-    
+    mov [fd_in], rax
     cmp rax, 0
     js failed_file_open
-    
-    ; TODO read file
-    
+
+    mov rdi, out_filename
+    mov rax, sys_open
+    mov rsi, O_TRUNC | O_CREAT | O_WRONLY
+    mov rdx, 644o     
+    syscall
+    mov [fd_out], rax
+    cmp rax, 0
+    js failed_file_open
+
+    ; TODO read file    
+
+    mov rdi, [fd_out]
+    call close_file
+
+    mov rdi, [fd_in]
+    call close_file 
 ;    printl goodbye_str, goodbye_str_len
                             
     xor rax, rax
     ret
-
+_nop:
+    ret
+close_file:
+    ;file descriptor in rbx
+    mov rax, sys_close
+    syscall
+    ret
 cout_str:
     ;str in rdi
     call strlen_o
@@ -110,8 +142,12 @@ cout_str_w_len:
     syscall
     ret
 failed_file_open:
-    PRINT_STRING failed_file_open_str
+    ;file name str in rdi
+    call cout_str
+    PRINT_STRING failed_file_open_str 
+        
     NEWLINE
+    
     call exit
 invalid_bit_depth:
     PRINT_STRING invalid_bit_depth_str
@@ -128,9 +164,18 @@ exit:
 get_filename:    
     ;argv in rbx
     ;argc in rcx
-    ;rsi will contain argv[0]
     cmp rcx,0
+    jz _nop
     
+    call get_filename_1
+        
+    dec rcx
+    add rbx, 8
+    jmp get_filename
+    
+    ret
+get_filename_1:
+    ;argv+i in rbx
     push rcx
     mov rdi, [rbx]
     call strlen_o
@@ -139,13 +184,9 @@ get_filename:
     mov rsi, rdi
     call cout_str_w_len
     NEWLINE
-    
+    ; TODO adjust rbx
     pop rcx
-    dec rcx
-    cmp rcx, 0
-    jnz too_many_args ; TODO set rsi to be argv[argc-1]
     ret
-get_filename_1
 strlen_o:
     ;string ptr in rdi
     push rbx
